86dcc77e4e352d25ec7e2615337da76c78bbde37
Rishabh  Sarup <rs868@cornell.edu>
2019-10-08 15:36:45 -0400

    added main function to run from command line

--- fastqparser.py
+++ fastqparser.py
@@ -1,31 +1,44 @@
-file = open("/home/local/CORNELL/public/hw1_files/Sample_Alignment.fastq", "r")
 
-lines = file.readlines()
-dna_lines = []
+def parser(file):
 
-for line in lines:
-    line=line.strip("\n")
+    lines = file.readlines()
+    dna_lines = []
 
-    num_a = line.count('A')
-    num_c = line.count('C')
-    num_g = line.count('G')
-    num_t = line.count('T')
+    for line in lines:
+        line=line.strip("\n")
 
-    if (num_a + num_c + num_g + num_t) == len(line): #ensures that the line only contains known DNA sequences, with no unknown DNA included
-        dna_lines.append(line)
+        num_a = line.count('A')
+        num_c = line.count('C')
+        num_g = line.count('G')
+        num_t = line.count('T')
+
+        if (num_a + num_c + num_g + num_t) == len(line): #ensures that the line only contains known DNA sequences, with no unknown DNA included
+            dna_lines.append(line)
 
 
-final_list = []
+    final_list = []
 
-for seq in dna_lines:
-    cgcontent = (seq.count('C')+seq.count('G'))/((seq.count('A'))+seq.count('T')+seq.count('G')+seq.count('C'))
-    reverse = []
-    i = len(seq)-1
-    while(i>=0):
-        reverse.append(seq[i])
-        i-=1
-    seq = (str(round(cgcontent, 3)) + "\t").join(reverse)
-    final_list.append(seq)
+    for seq in dna_lines:
+        cgcontent = (seq.count('C')+seq.count('G'))/((seq.count('A'))+seq.count('T')+seq.count('G')+seq.count('C'))
+        reverse = []
+        i = len(seq)-1
+        while(i>=0):
+            reverse.append(seq[i])
+            i-=1
+        seq = (str(round(cgcontent, 3)) + "\t").join(reverse)
+        final_list.append(seq)
 
-for line in sorted(final_list, reverse = True):
-    print (line)
+    for line in sorted(final_list, reverse = True):
+        print (line)
+
+def main():
+    parser = argparse.ArgumentParser(
+    description='Calculate sequence alignments for two sequences with a linear gap penalty.')
+    parser.add_argument('-f', action="store", dest="f", type=str, required=True)
+
+    args = parser.parse_args()
+    file = args.f
+    parser(file)
+
+if __name__ == "__main__":
+    main()
